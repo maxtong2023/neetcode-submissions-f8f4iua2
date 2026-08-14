de484a264e50f4f021faf4a3a43470719fbb962e
maxtong2023 <maxtong2027@u.northwestern.edu>
2026-08-14 11:27:47 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # can encode each of these into 26 length strings. Check if they are equal, and then group them together. 
+
+        hasher = {}
+        for word in strs: 
+            encoder = [0] * 26
+            for character in word: 
+                encoder[ord(character) - ord('a')] += 1
+            encoded = str(encoder)
+            hasher[encoded] = hasher.get(encoded, [])
+            hasher[encoded].append(word)
+        result = []
+
+        for key in hasher: 
+            result.append(hasher[key])
+        return result
